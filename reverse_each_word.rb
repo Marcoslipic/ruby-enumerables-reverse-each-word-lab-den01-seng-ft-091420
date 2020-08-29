36d7dd50a01d6559e286042520b49180cb7097c0
Marcos Lipic <mjlipic.37@gmail.com>
2020-08-29 19:01:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word (str)
   str_array = str.split(" ")
   puts str_array
   str_array.each do |word|
-    word.reverse_each_word
+    word.reverse!
   end 
   str_array.join(" ")
 end 
